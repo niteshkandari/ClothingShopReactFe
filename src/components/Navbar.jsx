791ed1e00472a38dd8023a4ea3045433fee4eaf1
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-24 19:45:25 +0530

    added responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCart } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  margin-bottom:10px;
+  ${mobile({height:'50px'})}
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  ${mobile({padding:'10px 0px'})};
 `;
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({display:'none'})}
+
 `;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -21,10 +27,13 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
+  ${mobile({marginLeft:'9px'})}
 `;
 const Input = styled.input`
   border: none;
   outline: none;
+  ${mobile({width:'50px'})}
+
 `;
 const Left = styled.div`
   flex: 1;
@@ -37,6 +46,8 @@ const Center = styled.div`
 `;
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({fontSize:'16px'})}
+
 `;
 const Right = styled.div`
   flex: 1;
@@ -44,11 +55,13 @@ const Right = styled.div`
   align-items: center;
   justify-content: flex-end;
   gap: 27px;
+  ${mobile({ flex:2, gap:'0px',justifyContent:'flex-start'})};
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
@@ -58,7 +71,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search..."/>
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
