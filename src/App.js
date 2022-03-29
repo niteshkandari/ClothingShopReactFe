67235a9b76f27e1f6aa00243b6890061f043dc67
nitesh.kandari <nitesh.kandari@azilen.com>
2022-03-29 20:00:50 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
@@ -7,7 +8,19 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 function App() {
-  return <Cart/>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/products/:category" component={ProductList}/>
+        <Route path="/product/:id" component={Product}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/cart" component={Cart}/>
+        {/* <Redirect to="/login" component={Login}/> */}
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
